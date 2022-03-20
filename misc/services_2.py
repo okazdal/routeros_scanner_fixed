292cd5a831bbcdcec6107aae31b7aeafaa5fd4be
Osman Kazdal <okazdal@sekuritim.com>
2022-03-20 23:35:42 +0300

    fixes

--- misc/services_2.py
+++ misc/services_2.py
@@ -6,14 +6,22 @@ def main():
 
     with paramiko.SSHClient() as ssh_client:
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect('192.168.1.1', username='admin', password='', port=22, look_for_keys=False,
+        ssh_client.connect('192.168.1.1', username='admin', password='0aberbyamd', port=22, look_for_keys=False,
                 allow_agent=False)
 
 
         stdin, stdout, stderr = ssh_client.exec_command('/ip service print detail')
-        data = stdout.read().decode('utf-8')
-        data = data.split('\n')
-        print(data)
+        data = str(stdout.read())
+        # print(data)
+
+        res = []
+
+        if ' 0 ' in data:
+            res = data.partition(' 0 ')[2].split('\\r\\n\\r\\n')[:-1]
+
+            print(res)
+
+
 
 
 
